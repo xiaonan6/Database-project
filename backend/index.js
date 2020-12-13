3e0c91ef14c7fbff567f2c4442f4ab243301c5fe
XIHE-git <69444835+XIHE-git@users.noreply.github.com>
2020-12-13 04:23:43 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/stateCases/:state', routes.getStateCasesByState); //for state block1: state cases
 
@@ -15,15 +15,21 @@ app.get('/statePolicy/:state', routes.getStatePolicy); //for state block1: state
 
 //app.get('/riskyCounties/:state', routes.getRiskyCounties); //this one can be added to state block1/ in the future
 
-app.get('/riskyStates', routes.get10riskyStates); //for state block2
-
 app.get('/countyCases/:state/:county', routes.getCountyCasesByCounty); // for county block1
+
+app.get('/countryDailyCases/:country', routes.getDailyWorldCases);//for world block 1
+
+app.get('/worldCases/:country', routes.getWorldCases);//for word block 1 total cases
+
+app.get('/riskyStates', routes.get10riskyStates); //for state block2
 
 app.get('/riskyCounties', routes.get10riskyCounties); //for county block2
 
+app.get('/riskyCountry', routes.get10RiskCountry);//for world block 2
+
 app.get('/USCases', routes.getUScases);
 
-app.get('/worldCases', routes.getWorldCases);
+
 
 
 
@@ -32,8 +38,11 @@ app.get('/allCaseState', routes.getConfirmCaseAllStates);
 
 app.get('/allDeathState', routes.getConfirmDeathAllStates);
 
+app.get('/allCaseWorld', routes.getConfirmCaseCountry);
 
-app.listen(8081, ()=> {
+app.get('/allDeathWorld', routes.getDealthsCaseCountry);
+
+app.listen(8081, () => {
     console.log('Server listening on PORT 8081');
 })
 
